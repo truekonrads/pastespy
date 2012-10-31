88242fe2a4c8562674628489f21a98453ed62900
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2012-10-31 18:04:09 +0000

    First working scrape

--- pastespy.py
+++ pastespy.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python
-
+from twisted.python import log
 from twisted.internet import reactor,defer
+from twisted.web.client import HTTPConnectionPool
+pool = HTTPConnectionPool(reactor, persistent=True)
+pool.maxPersistentPerHost = 5
 from treq import get
-from lib.pastebin import parsePastebinArchive
+import sys
+from lib.pastebin import parsePastebinArchive, parsePastebinIndividualPaste
+log.startLogging(sys.stdout)
+
+semaphore=defer.DeferredSemaphore(5)
+def semaphoreGet(*args,**kwargs):
+
+	d=semaphore.acquire().addCallback(lambda x: get(*args,**kwargs)).addCallback(semaphoreGetContinue)
+	return d
+
+def semaphoreGetContinue(d):
+	semaphore.release()
+	return d
 
 def handleArchivePage(response):
-	print "[*] Code is %s" % response.status_code
-	response.text.addCallback(parseArchiveResults)
+	log.msg( "Archive page code is %s" % response.status_code)
+	d=response.content.addCallback(parseArchiveResults)
+	return d
 
 def parseArchiveResults(text):
+	pages=[]
 	for (url,name) in parsePastebinArchive(text):
 		pasteID=url[1:]
-		print "`%s' has ID %s" % (name,pasteID)
-		fullPageDeferred=get("http://pastebin.com/%s", pasteID)
-		rawDeferred=get("http://pastebin.com/raw.php?i=%s" pasteID)
-		defer.DeferredList([fullPageDeferred,rawDeferred]).addCallback(handleIndividualPaste,pasteID)
-def handleIndividualPaste(paste)
-	print 
-	reactor.stop()
-d=get('http://pastebin.com/archive').addCallback(handleArchivePage)
+		log.msg("`%s' has ID %s" % (name,pasteID))
+		fullPageDeferred=semaphoreGet("http://pastebin.com/%s" % pasteID).addCallback(lambda r: r.content)
+		rawDeferred=semaphoreGet("http://pastebin.com/raw.php?i=%s" % pasteID).addCallback(lambda r: r.content)
+		d=defer.DeferredList([fullPageDeferred,rawDeferred]).addCallback(handleIndividualPaste,pasteID)
+		pages.append(d)
+	return d
+
+def handleIndividualPaste(dd,pasteID):
+	(fullPageResponse,rawPageResponse)=dd
+	if fullPageResponse[0] and rawPageResponse[0]:
+		# log.msg("DDDDDDD Type is " + str(fullPageResponse[1].__class__.__name__))
+		pageDetails=parsePastebinIndividualPaste(fullPageResponse[1])
+		pageDetails['snippet']=rawPageResponse[1].split("\n")[0]
+		log.msg("%(author)s on %(date)s posted the following about `%(title)s': %(snippet)s" % pageDetails)
+	
+d=semaphoreGet('http://pastebin.com/archive').addCallback(handleArchivePage).addBoth(lambda x: reactor.stop())
 reactor.run()
