0e366f59e7922ab00fa5ac410abb39115dd2c7cf
Konrads Smelkovs <konrads@mint-dev.(none)>
2013-08-13 11:41:56 +0100

    Update to MongoDB

--- pastespy.py
+++ pastespy.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 from twisted.python import log
+import logging
+import treq,txmongo
 from twisted.internet import reactor,defer
 from twisted.web.client import HTTPConnectionPool
 pool = HTTPConnectionPool(reactor, persistent=True)
@@ -9,17 +11,10 @@ import sys,re
 from lib.pastebin import parsePastebinArchive, parsePastebinIndividualPaste
 from lib.hashfinder import findHashesInRawPaste
 log.startLogging(sys.stdout)
+from twisted.internet.task import LoopingCall
 
-
-# def findHashesInRawPaste(paste):
-# 	for line in paste.split("\n"):
-# 		for word in re.split("[\S,:.\[\]]",line):
-# 			results,result_details=get_type_of(word,['win','web','unix','db','other'])
-# 			if(len(results['confident']) + len(results['likely']) + len(results['possible'])> 0):
-# 				for key in results.keys():
-# 					if(len(results[key]) > 0):
-# 						log.msg("Possible hash found: %s/%s"  %(key,results[key]))
-
+class DoghouseException(Exception):
+	pass
 semaphore=defer.DeferredSemaphore(5)
 def semaphoreGet(*args,**kwargs):
 
@@ -31,34 +26,83 @@ def semaphoreGetContinue(d):
 	return d
 
 def handleArchivePage(response):
-	log.msg( "Archive page code is %s" % response.status_code)
-	d=response.content.addCallback(parseArchiveResults)
+	log.msg( "Archive page code is %s" % response.code)
+	if response.code !=200:
+		log.msg("Oh oh - looks like we're in the doghouse")
+		raise DoghouseException("Couldn't get archive - code was %i" % response.code)
+	d=treq.content(response).addCallback(parseArchiveResults)
 	return d
 
 def parseArchiveResults(text):
-	pages=[]
-	for (url,name) in parsePastebinArchive(text):
-		pasteID=url[1:]
-		# log.msg("`%s' has ID %s" % (name,pasteID))
-		fullPageDeferred=semaphoreGet("http://pastebin.com/%s" % pasteID).addCallback(lambda r: r.content)
-		rawDeferred=semaphoreGet("http://pastebin.com/raw.php?i=%s" % pasteID).addCallback(lambda r: r.content)
-		d=defer.DeferredList([fullPageDeferred,rawDeferred]).addCallback(handleIndividualPaste,pasteID)
-		pages.append(d)
+	
+	pastes=[url[1:] for (url,name) in parsePastebinArchive(text)]
+	d=findUniquePastes(pastes)
+	d.addCallback(getIndividualPastes)
 	return d
+
+def getIndividualPastes(uniqueList):
+	pages=[]
+	log.msg("Fetching %i unique pastes" % len(uniqueList))
+	for pasteID in uniqueList:
+		fullPageDeferred=semaphoreGet("http://pastebin.com/%s" % pasteID).addCallback(treq.content)
+		rawDeferred=semaphoreGet("http://pastebin.com/raw.php?i=%s" % pasteID).addCallback(treq.content)
+		d=defer.DeferredList([fullPageDeferred,rawDeferred]).addCallback(handleIndividualPaste,pasteID)
+		pages.append(d)	
+	return defer.DeferredList(pages)
+	
 
 def handleIndividualPaste(dd,pasteID):
 	(fullPageResponse,rawPageResponse)=dd
 	if fullPageResponse[0] and rawPageResponse[0]:
-		# log.msg("DDDDDDD Type is " + str(fullPageResponse[1].__class__.__name__))
 		pageDetails=parsePastebinIndividualPaste(fullPageResponse[1])
-		rawPaste=rawPageResponse[1]
-		pageDetails['snippet']=rawPaste.split("\n")[0]
-		fixedPasteDetails={}
-		for (k,v) in pageDetails.items():
-			fixedPasteDetails[k]=v.decode('utf8','ignore')
+		try:
+			rawPaste=rawPageResponse[1].encode('utf8','ignore')
+			pageDetails['snippet']=rawPaste.split("\n")[0]
+			fixedPasteDetails={}
+			for (k,v) in pageDetails.items():
+				fixedPasteDetails[k]=v.encode('utf8','ignore')
+		except (UnicodeDecodeError,UnicodeEncodeError), e:
+			log.msg(str(e))
+			
 		log.msg("%(author)s on %(date)s posted the following about `%(title)s': %(snippet)s" % fixedPasteDetails)
-		hashwords=findHashesInRawPaste(rawPaste,["confident"])
+		hashwords=findHashesInRawPaste(rawPaste,["confident"])		
 		for h in hashwords:
 			log.msg("%s found in pastebin ID: %s" % (h,pasteID))
-d=semaphoreGet('http://pastebin.com/archive').addCallback(handleArchivePage).addBoth(lambda x: reactor.stop())
-reactor.run()
+		return storePaste(pasteID,fixedPasteDetails,rawPaste,hashwords)
+
+def findUniquePastes(archiveList):
+	query={'pasteID' : { '$in' : archiveList}}
+	d=getMongoConnection().addCallback(lambda cnx: cnx.pastebin.pastes.find(query,fields=['pasteID']))
+	d.addCallback(lambda res: set(archiveList) - set([x["pasteID"] for x in res]))
+	return d
+	
+def storePaste(pasteID,details,raw,hashwords):
+	data=details.copy()
+	data['pasteID']=pasteID
+	data['raw']=raw
+	data['hashwords']=hashwords
+	d=getMongoConnection()
+	return d.addCallback(lambda cnx: cnx.pastebin.pastes.insert(data))
+
+_connection=None
+def getMongoConnection():
+	if _connection:
+		log.msg("Returning an existing Mongo connection",logLevel=logging.DEBUG)
+		return defer.succeed(_connection)
+	else:
+		d=txmongo.MongoConnection()
+		return d.addCallback(storeMongoConnection)
+
+def storeMongoConnection(cnx):
+	_connection=cnx
+	return defer.succeed(_connection)
+
+def runIteration():
+	d=semaphoreGet('http://pastebin.com/archive')
+	d.addCallback(handleArchivePage)
+	d.addErrback(log.err)
+	return d
+	#d.addBoth(lambda x: reactor.stop())
+loop=LoopingCall(runIteration)
+loop.start(20,now=True)
+reactor.run() 
